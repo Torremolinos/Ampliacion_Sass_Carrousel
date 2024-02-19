be09470e06e41ead3aa06c295ed57ecccbbf8511
Torremolinos <adrian_2054@hotmail.com>
2024-02-19 21:00:06 +0100

    Acabado

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -73,4 +73,19 @@ const validar = (e) => {
         nav.classList.remove("visible");
     })
     
-    
+     const btn = document.querySelector(".btn-toggle");
+
+        const currentTheme = localStorage.getItem("theme");
+        if (currentTheme == "dark") {
+            document.body.classList.add("dark-theme");
+        }
+
+        btn.addEventListener("click", function () {
+            document.body.classList.toggle("dark-theme");
+
+            let theme = "light";
+            if (document.body.classList.contains("dark-theme")) {
+                theme = "dark";
+            }
+            localStorage.setItem("theme", theme);
+        });
